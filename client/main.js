eda93d82edea0d6fe87674996b66f6da2db71a11
kongsh <ksh080838@gmail.com>
2024-11-20 09:36:52 +0900

    JS 17일차
    calculator

--- client/main.js
+++ client/main.js
@@ -2,7 +2,13 @@
 // import { insertLast } from './lib/dom/insert.js';
 // import clearContents from './lib/dom/clearContents.js';
 
-import { getNode, getNodes, insertLast, clearContents } from './lib/index.js';
+import {
+  getNode as $,
+  getNodes,
+  typeError,
+  insertLast,
+  clearContents,
+} from './lib/index.js';
 
 // const first = getNode('#firstNumber');
 // const second = getNode('#secondNumber');
@@ -31,18 +37,56 @@ import { getNode, getNodes, insertLast, clearContents } from './lib/index.js';
 // 4. 숫자 더하기
 // 5. result에 출력하기
 
-const first = getNode('#firstNumber');
-const second = getNode('#secondNumber');
-const result = getNode('.result');
+function phase1() {
+  const first = $('#firstNumber');
+  const second = $('#secondNumber');
+  const clear = $('#clear');
+  const result = $('.result');
 
-function handleInput() {
-  const firstValue = Number(first.value);
-  const secondValue = +second.value;
-  const total = firstValue + secondValue;
+  function handleInput() {
+    const firstValue = Number(first.value);
+    const secondValue = +second.value;
+    const total = firstValue + secondValue;
 
-  clearContents(result);
-  insertLast(result, total);
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    clearContents(first);
+    clearContents(second);
+    result.textContent = '-';
+  }
+
+  first.addEventListener('input', handleInput);
+  second.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
 }
 
-first.addEventListener('input', handleInput);
-second.addEventListener('input', handleInput);
+function phase2() {
+  const calculator = $('.calculator');
+  const result = $('.result');
+  const clear = $('#clear');
+  const numberInputs = [...document.querySelectorAll('input:not(#clear)')];
+
+  function handleInput() {
+    const total = numberInputs.reduce((acc, cur) => acc + +cur.value, 0);
+
+    clearContents(result);
+    insertLast(result, total);
+  }
+
+  function handleClear(e) {
+    e.preventDefault();
+
+    numberInputs.forEach(clearContents);
+    result.textContent = '-';
+  }
+
+  calculator.addEventListener('input', handleInput);
+  clear.addEventListener('click', handleClear);
+}
+
+phase2();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-const types = {
-  isObject: true,
-  isArray: true,
-  isString: true,
-  isNumber: true,
-  isNull: true,
-  isUndefined: true,
-  isFunction: true,
-};
-
-const dom = {
-  getNode: true,
-  getAttr: true,
-  setAttr: true,
-  attr: true,
-  insertLast: true,
-};
-
-const error = {
-  typeError: true,
-  syntaxError: true,
-  refError: true,
-};
-
 export default [
   pluginJs.configs.recommended,
   {
@@ -32,9 +8,6 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...types,
-        ...dom,
-        ...error,
       },
     },
     rules: {
