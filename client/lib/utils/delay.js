35c71aa1235fd2ab3b67fbcaa2f9573ed6acbaef
kongsh <ksh080838@gmail.com>
2024-11-21 16:45:11 +0900

    JS 18일차
    callback, xhr, promise

---
+++ client/lib/utils/delay.js
@@ -0,0 +1,87 @@
+import { getNode } from '../dom/getNode.js';
+import { isNumber, isObject } from './type.js';
+
+function delay(callback, timeout = 1000) {
+  setTimeout(callback, timeout);
+}
+
+const first = getNode('.first');
+const second = getNode('.second');
+
+// delay(()=>{
+//   first.style.top = '-100px';
+
+//   delay(()=>{
+//     first.style.transform = 'rotate(360deg)'
+
+//     delay(()=>{
+//       first.style.top = '0px';
+//       second.style.top = '0px';
+//     })
+
+//     second.style.transform = 'rotate(360deg)'
+//   })
+//   second.style.top = '100px';
+// })
+const shouldRejected = false;
+
+const p = new Promise((성공, 실패) => {
+  if (!shouldRejected) {
+    성공('성공입니다~');
+  } else {
+    실패('실패입니다~');
+  }
+});
+
+p.then((res) => {
+  // console.log( res );
+});
+
+// promise 객체를 반환하는 함수 => 재사용
+
+const defaultOptions = {
+  shouldRejected: false,
+  data: '성공',
+  errorMessage: '알 수 없는 오류',
+  timeout: 1000,
+};
+
+function delayP(options) {
+  let config = { ...defaultOptions };
+
+  if (isNumber(options)) {
+    config.timeout = options;
+  }
+
+  if (isObject(options)) {
+    config = { ...defaultOptions, ...options };
+  }
+
+  const { shouldRejected, data, errorMessage, timeout } = config;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!shouldRejected) {
+        resolve(data);
+      } else {
+        reject(errorMessage);
+      }
+    }, timeout);
+  });
+}
+
+delayP(2000);
+
+// delayP(false)
+//   .then((res) => {
+//     first.style.top = '-100px';
+//     return delayP(false);
+//   })
+//   .then((res) => {
+//     first.style.transform = 'rotate(360deg)';
+//     return delayP(false);
+//   })
+//   .then((res) => {
+//     first.style.top = '0px';
+//     return delayP(false);
+//   });
---
+++ client/lib/utils/xhr.js
@@ -0,0 +1,164 @@
+const END_POINT = `https://jsonplaceholder.typicode.com/users`;
+
+// [readyState]
+// 0 : uninitialized
+// 1 : loading
+// 2 : loaded
+// 3 : interactive
+// 4 : complete  => 성공 | 실패
+
+/* -------------------------------------------- */
+/*                   callback                   */
+/* -------------------------------------------- */
+
+function xhr({
+  method = 'GET',
+  url = '',
+  success = null,
+  fail = null,
+  body = null,
+  headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+} = {}) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+
+  if (!(method === 'DELETE')) {
+    Object.entries(headers).forEach(([k, v]) => {
+      xhr.setRequestHeader(k, v);
+    });
+  }
+
+  xhr.addEventListener('readystatechange', () => {
+    const { status, response, readyState } = xhr;
+    if (readyState === 4) {
+      if (status >= 200 && status < 400) {
+        const data = JSON.parse(response);
+        success(data);
+      } else {
+        fail({ message: '실패' });
+      }
+    }
+  });
+
+  xhr.send(JSON.stringify(body));
+}
+
+xhr.get = (url, success, fail) => {
+  xhr({ url, success, fail });
+};
+
+xhr.post = (url, body, success, fail) => {
+  xhr({
+    method: 'POST',
+    url,
+    body,
+    success,
+    fail,
+  });
+};
+
+xhr.put = (url, body, success, fail) => {
+  xhr({
+    method: 'PUT',
+    url,
+    body,
+    success,
+    fail,
+  });
+};
+
+xhr.delete = (url, success, fail) => {
+  xhr({
+    method: 'DELETE',
+    url,
+    success,
+    fail,
+  });
+};
+
+/* -------------------------------------------- */
+/*                    promise                   */
+/* -------------------------------------------- */
+
+// mixin
+const defaultOptions = {
+  method: 'GET',
+  url: '',
+  body: null,
+  errorMessage: '서버와의 통신이 원활하지 않습니다.',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
+function xhrPromise(options = {}) {
+  const { method, url, errorMessage, body, headers } = {
+    ...defaultOptions,
+    ...options,
+    headers: {
+      ...defaultOptions.headers,
+      ...options.headers,
+    },
+  };
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+
+  if (!(method === 'DELETE')) {
+    Object.entries(headers).forEach(([k, v]) => {
+      xhr.setRequestHeader(k, v);
+    });
+  }
+
+  xhr.send(body ? JSON.stringify(body) : null);
+
+  return new Promise((resolve, reject) => {
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status >= 200 && xhr.status < 400) {
+          resolve(JSON.parse(xhr.response));
+        } else {
+          reject({ message: 'data 통신이 원활하지 않습니다.' });
+        }
+      }
+    });
+  });
+}
+
+// xhrPromise({
+//   method: 'GET',
+//   url: END_POINT,
+// })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+xhrPromise.get = (url) => xhrPromise({ url });
+
+xhrPromise.delete = (url) => xhrPromise({ method: 'DELETE', url });
+
+xhrPromise.post = (url, body) => xhrPromise({ method: 'POST', url, body });
+
+xhrPromise.put = (url, body) => xhrPromise({ method: 'PUT', url, body });
+
+xhrPromise
+  .get(END_POINT)
+  .then((res) => {
+    res.forEach(({ website }) => {
+      const tag = `
+      <div>site : ${website}</div>
+    `;
+      document.body.insertAdjacentHTML('beforeend', tag);
+    });
+  })
+  .then(() => {})
+  .catch(() => {});
