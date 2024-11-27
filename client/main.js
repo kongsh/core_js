b2f99eb239b4efd76dec8b9bd7607e32d778c864
kongsh <ksh080838@gmail.com>
2024-11-27 16:09:58 +0900

    JS 22일차
    figma를 활용해 Card, Counter Component 제작

--- client/main.js
+++ client/main.js
@@ -1,13 +1,17 @@
 import { Button } from './components/Button/Button.js';
+import { Card } from './components/Card/Card.js';
+import { Counter } from './components/Counter/Counter.js';
 
 const app = document.getElementById('app');
 
 function defineElements() {
   customElements.define('custom-button', Button);
+  customElements.define('custom-card', Card);
+  customElements.define('custom-counter', Counter);
 }
 
 defineElements();
 
 const buttonElement = document.createElement('custom-button');
 
-app.append(buttonElement);
+// app.append(buttonElement);
