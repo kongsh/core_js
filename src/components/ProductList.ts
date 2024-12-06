62c8b956628f0d7a970d4921ccd448ecccb160cf
kongsh <ksh080838@gmail.com>
2024-12-06 16:04:18 +0900

    JS 29일차
    detail, newpost 컴포넌트

--- src/components/ProductList.ts
+++ src/components/ProductList.ts
@@ -1,13 +1,15 @@
 import { LitElement, html, css, CSSResultGroup, PropertyDeclarations } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 import resetCSS from "../Layout/resetCSS";
 import { getPbImageURL } from "../api/getPbImageURL";
-import { Item } from "../@types/type";
+import { Auth, Item } from "../@types/type";
 import gsap from "gsap";
 
 @customElement("product-list")
 class ProductList extends LitElement {
   @property({ type: Object }) data = { items: [], page: 0, perPage: 0, totalItems: 0, totalPages: 0 };
+
+  @state() loginData = {} as Auth;
 
   static styles: CSSResultGroup = [
     resetCSS,
@@ -57,6 +59,17 @@ class ProductList extends LitElement {
           }
         }
       }
+
+      .new-post {
+        padding: 0.5rem 1rem;
+        background-color: dodgerblue;
+        color: white;
+        border-radius: 20px;
+        position: fixed;
+        transform: translateX(-50%);
+        left: 50%;
+        bottom: 2rem;
+      }
     `,
   ];
 
@@ -71,6 +84,8 @@ class ProductList extends LitElement {
     const data = await response.json();
 
     this.data = data;
+
+    this.loginData = JSON.parse(localStorage.getItem("auth") ?? "{}");
   }
 
   updated(changenProperties: Map<string | number | symbol, unknown>): void {
@@ -88,13 +103,15 @@ class ProductList extends LitElement {
   }
 
   render() {
+    const { isAuth } = this.loginData;
+
     return html`
       <div class="container">
         <ul>
           ${this.data.items.map(
             (item: Item) => html`
               <li class="product-item">
-                <a href="/">
+                <a href=${isAuth ? `/src/pages/detail/index.html?product=${item.id}` : `/`}>
                   <figure>
                     <img src="${getPbImageURL(item)}" alt="" />
                   </figure>
@@ -111,6 +128,7 @@ class ProductList extends LitElement {
           )}
         </ul>
       </div>
+      <a class="new-post" href="/src/pages/newpost/">+ 상품 추가</a>
     `;
   }
 }
